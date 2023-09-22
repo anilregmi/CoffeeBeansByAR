233370fd8c3891fdc6e4e15282a2b8144162ecee
katmaticdev2 <katmaticdev2@gmail.com>
2023-09-22 14:24:36 +0545

    Updated the DataBinding Property to Transfer the UI Logic to ViewModel

--- CoffeeBeans/ViewModel/CustomersViewModel.cs
+++ CoffeeBeans/ViewModel/CustomersViewModel.cs
@@ -17,6 +17,7 @@ namespace CoffeeBeans.ViewModel
 
         public ObservableCollection<Customer> Customers { get; } = new();
 
+        public Customer? SelectedCustomer{ get; set; }
         public async Task LoadAsync()
         {
             if (Customers.Any())
